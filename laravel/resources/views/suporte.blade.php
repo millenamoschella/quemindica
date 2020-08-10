1e5f195d18cc7c5d49cd45d7ce236ae3c66c05ae
millenamoschella@gmail.com <millenamoschella@users.noreply.github.com>
2020-08-10 18:43:48 -0300

    Ajustes página de suporte

--- laravel/resources/views/suporte.blade.php
+++ laravel/resources/views/suporte.blade.php
@@ -3,55 +3,65 @@
 @section('content')
 
     {{-- FORMULÁRIO SUPORTE --}}
-    <section class="secao-formulario">
+    <section class="secao-formulario text-center">
 
-        <div class="container-formulario-suporte">
-            <div class="wrap-formulario">
+        <div class="card d-flex justify-content-center div-formulario">
+            <div class="row">
 
-                {{-- IMAGEM --}}
-                <div class="banner-suporte">
-                    <img src="{{ asset('../imagens/institucional/banner02.jpg') }}" alt="Banner Suporte">
-                </div>
+                {{-- BANNER --}}
+                <div class="col-lg-6 col-md-12 col-sm-12">
+                    <img src="{{ asset('../imagens/institucional/suporte-novo-2.png') }}" width="100%" height="auto"> </div>
 
                 {{-- TÍTULO --}}
-                <form class="formulario">
-                    <h2>
-                        Suporte
-                    </h2>
+                <div class="col-lg-6">
+                    <h4 class="titulo">Suporte</h4>
 
-                    {{-- NOME --}}
-                    <div class="wrap-area-input">
-                        <input class="area-input" type="text" name="name" placeholder="Nome">
-                        <span class="focus-area-input"></span>
-                        <span class="icone-input">
-                            <i class="fa fa-user" aria-hidden="true"></i>
-                        </span>
-                    </div>
+                    {{-- FORMULÁRIO --}}
+                    <form class="container">
 
-                    {{-- EMAIL --}}
-                    <div class="wrap-area-input">
-                        <input class="area-input" type="text" name="email" placeholder="Email">
-                        <span class="focus-area-input"></span>
-                        <span class="icone-input">
-                            <i class="fa fa-envelope" aria-hidden="true"></i>
-                        </span>
-                    </div>
+                        {{-- NOME --}}
+                        <div class="row">
+                            <div class="form-group col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                <input class="area-input" type="text" name="name" placeholder="Nome">
+                                <span class="focus-area-input"></span>
+                                <span class="icone-input">
+                                    <i class="fa fa-user" aria-hidden="true"></i>
+                                </span>
+                            </div>
+                        </div>
 
-                    {{-- MENSAGEM --}}
-                    <div class="wrap-area-input">
-                        <textarea class="area-input" name="message" placeholder="Mensagem"></textarea>
-                        <span class="focus-area-input"></span>
-                    </div>
+                        {{-- EMAIL --}}
+                        <div class="row">
+                            <div class="form-group col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                <input class="area-input" type="text" name="email" placeholder="Email">
+                                <span class="focus-area-input"></span>
+                                <span class="icone-input">
+                                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                                </span>
+                            </div>
+                        </div>
 
-                    {{-- BOTÃO --}}
-                    <div class="container-botao">
-                        <button class="btn-formulario">
-                            Enviar
-                        </button>
-                    </div>
+                        {{-- MENSAGEM --}}
+                        <div class="row">
+                            <div class="form-group col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                <div class="wrap-area-input">
+                                    <textarea class="area-input" name="message" placeholder="Mensagem"></textarea>
+                                    <span class="focus-area-input"></span>
+                                </div>
+                            </div>
+                        </div>
 
+                        {{-- BOTÃO --}}
+                        <div class="row">
+                            <div class="form-group col-lg-12">
+                                <button type="submit" class="btn btn-formulario">
+                                    <span>Enivar</span>
+                                </button>
+                            </div>
 
-                </form>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
 
