a46e2464f41452f6e610dc6018e6c5143d3874f3
millenamoschella@gmail.com <millenamoschella@users.noreply.github.com>
2020-08-16 22:06:38 -0300

    Ajuste Footer

--- laravel/resources/views/layouts/footer.blade.php
+++ laravel/resources/views/layouts/footer.blade.php
@@ -1,63 +1,108 @@
-<footer class="page-footer text-center footer">
+<footer>
+    <div class="secao-footer">
+        <div class="container">
+            <div class="row text-center">
 
-    <div class="container-fluid text-md-left ">
-        <div class="row">
 
-            {{-- ASIDE ESQUERDO --}}
-            <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
-                <ul class="list-unstyled">
-                    <li>
-                        <p><a href="{{ url('sobre') }}">Quem Somos</a></p>
-                    </li>
-                    <li>
-                        <p><a href="{{ url('suporte') }}">Suporte</a></p>
-                    </li>
-                    <li>
-                        <p><a href="{{ url('faq') }}">FAQ</a></p>
-                    </li>
-                    <li>
-                        <p><a href="{{ url('politicas-termos') }}">Termos de Uso</a></p>
-                    </li>
-                    <li>
-                        <p><a href="{{ url('politicas-termos') }}">Politica de Dados</a></p>
-                    </li>
-                    <li>
-                        <p><a href="{{ url('politicas-termos') }}">Politica de Cookies</a></p>
-                    </li>
-                </ul>
+                {{-- MENU --}}
+                <div class="col-lg-3 col-md-6">
+
+                    <h5>Navegação</h5>
+
+                    <div class="menu-footer">
+                        <p>
+                            <a href="{{ url('sobre') }}">Quem Somos</a>
+                        </p>
+                        <p>
+                            <a href="{{ url('suporte') }}">Suporte</a>
+                        </p>
+                        <p>
+                            <a href="{{ url('faq') }}">FAQ</a>
+                        </p>
+                        <p>
+                            <a href="{{ url('politicas-termos') }}">Termos de Uso</a>
+                        </p>
+                        <p>
+                            <a href="{{ url('politicas-termos') }}">Política de Dados</a>
+                        </p>
+                        <p>
+                            <a href="{{ url('politicas-termos') }}">Política de Cookies</a>
+                        </p>
+                    </div>
+                </div>
+
+
+                {{-- NEWSLETTER --}}
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="mb-3">Fique por dentro!</h5>
+
+                    <div class="footer-assinar">
+                        <form action="#" method="post">
+                            <input type="email" name="email" class="form-control" placeholder="Email">
+                            <button class="btn botao-assinar mt-4" type="submit">Assinar</button>
+                        </form>
+                    </div>
+                </div>
+
+
+                {{-- LOCALIZAÇÃO --}}
+                <div class="col-lg-3 col-md-6 ">
+
+                    <h5>Localização</h5>
+
+                    <div class="localizacao-footer">
+                        <ul class="list-unstyled">
+                            <li>
+                                <p> São Paulo, 01010-010</p>
+                            </li>
+                            <li>
+                                <p> contato@quemindica.com.br</p>
+                            </li>
+                            <li>
+                                <p> + 55 11 0000-000</p>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                {{-- MÍDIAS SOCIAIS --}}
+                <div class="col-lg-3 col-md-6">
+
+                    <h5>Na Web</h5>
+
+                    <div class="social-media">
+                        {{-- FACEBOOK --}}
+                        <a href="https://www.facebook.com" target="_blank">
+                            <i class="fa fa-facebook-official fa-lg white-text"></i>
+                        </a>
+
+                        {{-- INSTAGRAM --}}
+                        <a href="https://www.instagram.com" target="_blank">
+                            <i class="fa fa-instagram fa-lg white-text"></i>
+                        </a>
+
+                        {{-- LINKEDIN --}}
+                        <a href="https://www.linkedin.com" target="_blank">
+                            <i class="fa fa-linkedin-square fa-lg white-text"></i>
+                        </a>
+
+                        {{-- WHATSAPP --}}
+                        <a href="https://wa.me/5511900000000" target="_blank">
+                            <i class="fa fa-whatsapp fa-lg white-text"></i>
+                        </a>
+                    </div>
+
+                </div>
+
             </div>
-
-            {{-- LOCALIZAÇÃO --}}
-            <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
-                <h6 class="font-weight-bold text-uppercase mb-4">Localização</h6>
-                <ul class="list-unstyled endereco">
-                    <li>
-                        <p> São Paulo, 01010-010</p>
-                    </li>
-                    <li>
-                        <p> contato@quemindica.com.br</p>
-                    </li>
-                    <li>
-                        <p> + 55 11 0000-000</p>
-                    </li>
-                </ul>
-            </div>
-
-            {{-- ASIDE DIREIRO --}}
-            <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
-                <h6 class="font-weight-bold text-uppercase mb-4">Na Web</h6>
-                <a href="https://www.facebook.com" target="_blank"><img src="{{ asset('imagens/midias/facebook.png') }}"
-                        alt="Facebook"></a>
-                <a href="https://www.instagram.com" target="_blank"><img
-                        src="{{ asset('imagens/midias/instagram.png') }}" alt=" Instagram"></a>
-                <a href="https://wa.me/5511900000000" target="_blank"><img
-                        src="{{ asset('imagens/midias/whatsapp.png') }}" alt="Whatsapp"></a>
-            </div>
-
-            {{-- DIREIRO AUTORAIS --}}
-            <p class="copyright">Quem Indica &copy; <?php echo date('Y'); ?> - Todos os
-                direitos reservados</p>
-
         </div>
+    </div>
+
+    {{-- COPYRIGHT --}}
+    <div class="footer-copyright text-center">
+        <p class="copyright">Quem Indica</a> &copy; <?php echo date('Y'); ?> - Todos os
+            direitos reservados</p>
+    </div>
 
 </footer>
